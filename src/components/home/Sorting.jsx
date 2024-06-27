c5d1d4e0c9345a116831e7997b9820184af13857
Samet Aslan <sametaslan1710@gmail.com>
2024-06-27 05:27:50 +0300

    add: basic structure for Sorting.jsx

--- src/components/home/Sorting.jsx
+++ src/components/home/Sorting.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Sorting = () => {
   return (
-    <div className='bg-gray-100 my-5 p-5 flex items-center justify-end'>Sorting</div>
-  )
-}
+    <div className="bg-gray-100 my-5 p-5 flex items-center justify-end">
+      <select className="bg-white py-3 px-5" name="" id="">
+        <option disabled value="">
+          Select
+        </option>
+        <option value="inc">Increment</option>
+        <option value="dec">Decrement</option>
+      </select>
+    </div>
+  );
+};
 
-export default Sorting
+export default Sorting;
