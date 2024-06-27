1d1caeefbf3a6cdcea72794f569081608f97320f
Samet Aslan <sametaslan1710@gmail.com>
2024-06-27 05:24:58 +0300

    add: Basic structure of Home folder

---
+++ src/components/home/Sorting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sorting = () => {
+  return (
+    <div className='bg-gray-100 my-5 p-5 flex items-center justify-end'>Sorting</div>
+  )
+}
+
+export default Sorting
