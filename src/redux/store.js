996c227996b1fca26c9f68bce300b5eaae56a200
Samet Aslan <sametaslan1710@gmail.com>
2024-06-27 06:08:00 +0300

    add: productSlice.js create: productDetail fetching via rtk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./categorySlice";
+import productSlice from "./productSlice";
 
 
 
 export const store = configureStore({
     reducer: {
-        categories: categorySlice
+        categories: categorySlice,
+        products: productSlice
     },
 })
